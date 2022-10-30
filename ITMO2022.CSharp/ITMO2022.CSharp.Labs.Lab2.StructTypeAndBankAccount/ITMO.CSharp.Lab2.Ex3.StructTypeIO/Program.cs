b4ec245fd8baddf617d8d49975ab3c6976e85a1d
Aleksei Allen <aqml@mail.ru>
2022-10-30 20:40:59 +0300

    StructTypeIO added

---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab2.StructTypeAndBankAccount/ITMO.CSharp.Lab2.Ex3.StructTypeIO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab2.Ex3.StructTypeIO
+{
+    public enum AccountType { Checking, Deposit }
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+
+
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            //goldAccount.accNo = 123;
+            Console.WriteLine("Enter account number: ");
+            goldAccount.accNo = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Acct NUmber {0}", goldAccount.accNo);
+            Console.WriteLine("Acc Type {0}", goldAccount.accType);
+            Console.WriteLine("Acct Balance ${0}", goldAccount.accBal);
+
+            Console.ReadKey();
+        }
+    }
+}
