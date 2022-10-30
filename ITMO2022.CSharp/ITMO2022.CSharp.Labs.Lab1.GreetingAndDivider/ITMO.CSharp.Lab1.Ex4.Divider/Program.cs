91668d93fc7fe683b81e435f2dd185ae01510cc7
Aleksei Allen <aqml@mail.ru>
2022-10-30 17:00:26 +0300

    updated

---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab1.GreetingAndDivider/ITMO.CSharp.Lab1.Ex4.Divider/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab1.Ex4.Divider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
