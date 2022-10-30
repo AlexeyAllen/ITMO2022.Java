ffb5281cc00eec4f889e1dc1a141c8301f87d611
Aleksei Allen <aqml@mail.ru>
2022-10-30 16:52:55 +0300

    updated

--- ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab1.GreetingAndDivider/ITMO.CSharp.Lab1.Ex1.Greeting/Program.cs
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab1.GreetingAndDivider/ITMO.CSharp.Lab1.Ex1.Greeting/Program.cs
@@ -10,6 +10,13 @@ namespace ITMO.CSharp.Lab1.Ex1.Greeting
     {
         static void Main(string[] args)
         {
+            string myName;
+
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", myName);
+
+            Console.ReadKey();
         }
     }
 }
