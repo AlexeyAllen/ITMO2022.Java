da31700d13c3309f727fd2a25684f653b3030f03
Aleksei Allen <aqml@mail.ru>
2022-10-30 16:49:03 +0300

    das

---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab1.GreetingAndDivider/ITMO.CSharp.Lab1.Ex1.Greeting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab1.Ex1.Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
