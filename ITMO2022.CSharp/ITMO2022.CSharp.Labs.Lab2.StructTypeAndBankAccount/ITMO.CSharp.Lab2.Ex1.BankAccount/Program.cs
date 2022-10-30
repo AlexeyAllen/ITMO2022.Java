78e810cf1002f8253c31c5ec7cf7a2f3669b9519
Aleksei Allen <aqml@mail.ru>
2022-10-30 20:30:54 +0300

    BankAccount proj added

---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab2.StructTypeAndBankAccount/ITMO.CSharp.Lab2.Ex1.BankAccount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab2.Ex1.BankAccount
+{
+    public enum AccountType { Checking, Deposit }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+
+            Console.ReadKey();
+        }
+    }
+}
