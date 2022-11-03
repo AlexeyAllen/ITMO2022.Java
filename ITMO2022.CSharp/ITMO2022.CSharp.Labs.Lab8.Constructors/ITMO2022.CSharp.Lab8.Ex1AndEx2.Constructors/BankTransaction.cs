1d41d907581fa69abe5e9ac7bf04aecc5bf8425b
Aleksei Allen <aqml@mail.ru>
2022-11-03 09:16:56 +0300

    Lab 8 added

---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab8.Constructors/ITMO2022.CSharp.Lab8.Ex1AndEx2.Constructors/BankTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+
+    public decimal Amount() { return amount; }
+    public DateTime When() { return when; }
+
+    public BankTransaction(decimal tranAmount)
+    {
+        amount = tranAmount;
+        when = DateTime.Now;
+    }
+}
