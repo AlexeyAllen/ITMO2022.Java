9cc982d0b3317cbe1ddce842ee01840b2cac9709
Aleksei Allen <aqml@mail.ru>
2022-11-09 22:12:08 +0500

    Lab 11 added

---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab11.BankAccountAndBankTransactionAndIndexer/ITMO2022.CSharp.Lab11.Ex1AndEx2AndEx3/AUDIT/Audit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Banking
+{
+    public class Audit
+    {
+        private string filename;
+        private StreamWriter auditFile;
+        private bool closed = false;
+
+        public Audit(string fileToUse)
+        {
+            filename = fileToUse;
+            auditFile = File.AppendText(fileToUse);
+        }
+
+        public void RecordTransaction(object sender, AuditEventArgs eventData)
+        {
+            BankTransaction tempTrans = eventData.GetTransaction();
+
+            if (tempTrans != null)
+                auditFile.WriteLine("Amount: {0}\tDate: {1}", tempTrans.Amount(), tempTrans.When()); 
+        }
+
+        public void Close()
+        {
+            if (!closed)
+            {
+                auditFile.Close();
+                closed = true;
+            }
+        }
+    }
+}
---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab11.BankAccountAndBankTransactionAndIndexer/ITMO2022.CSharp.Lab11.Ex1AndEx2AndEx3/AUDIT/AuditEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    public class AuditEventArgs : EventArgs
+    {
+        private readonly BankTransaction transData = null;
+
+        public AuditEventArgs(BankTransaction transaction)
+        {
+            transData = transaction;
+        }
+
+        public BankTransaction GetTransaction()
+        {
+            return transData;
+        }
+    }
+}
