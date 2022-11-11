36b72379bf5d71d6eb585a7732fb722574a41ff4
Aleksei Allen <aqml@mail.ru>
2022-11-11 21:49:03 +0500

    Lab13 added

---
+++ ITMO2022.CSharp/ITMO2022.CSharp.Labs.Lab13.ConditionalAndDeveloperInfoAttribute/ITMO2022.CSharp.Lab13.Ex2.DeveloperInfoAttribute/CustomAttribute/CustomAttribute/DeveloperInfoAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Struct, AllowMultiple = true)]
+
+    public class DeveloperInfoAttribute : System.Attribute
+    {
+        private string developerName;
+        private string dateCreated;
+
+        public DeveloperInfoAttribute(string developerName)
+        {
+            this.developerName = developerName;
+        }
+
+        public string Developer
+        {
+            get
+            {
+                return developerName;
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return dateCreated;
+            }
+            set
+            {
+                dateCreated = value;
+            }
+
+        }
+    }
+}
