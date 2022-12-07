9f0a26f8e6f64802f71031f5e68ce6c427c950d5
Aleksei Allen <aqml@mail.ru>
2022-12-07 20:12:47 +0300

    Lab1 Ex 3 added

---
+++ ITMO2022.CSharp.Forms/ITMO2022.CSharp.Forms.Lab01.Exercise03. Inheritance/nForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO2022.CSharp.Forms.Lab01.Exercise03._Inheritance
+{
+    public partial class nForm : ITMO2022.CSharp.Forms.Lab01.Exercise03._Inheritance.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
