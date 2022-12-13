f29bca8d6d06a8a6cfcab4a28b39a7be34bb3abd
Aleksei Allen <aqml@mail.ru>
2022-12-13 09:49:31 +0300

    Lab 3 Ex 2, 3 added

---
+++ ITMO2022.CSharp.Forms/ITMO2022.CSharp.Forms.Lab03.Ex2.WinTimer2/Form1.cs
@@ -0,0 +1,10 @@
+namespace ITMO2022.CSharp.Forms.Lab03.Ex1.WinTimer2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ITMO2022.CSharp.Forms/ITMO2022.CSharp.Forms.Lab03.Ex3.WinButNum/Form1.cs
@@ -0,0 +1,10 @@
+namespace ITMO2022.CSharp.Forms.Lab03.Ex3.WinButNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
