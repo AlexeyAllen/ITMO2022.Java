bdaeeb26312a5ff856e916206e01ee251d6ec8cd
Aleksei Allen <aqml@mail.ru>
2022-12-30 22:17:38 +0500

    Lab 1 ExpenseIt completed

---
+++ ITMO2022.CSharp.WPF/ITMO2022.CSharp.WPF.Lab11.Ex02.InkPad/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITMO2022.CSharp.WPF.Lab11.Ex02.InkPad
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
