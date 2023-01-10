803d526fed550c991b5c66a5038baa19ee35187a
Aleksei Allen <aqml@mail.ru>
2023-01-10 21:33:47 +0500

    HTML added

---
+++ ITMO2022.CSharp.WPF/ITMO2022.CSharp.WPF.MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf_MVVM.Model;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace Wpf_MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        #region Constructor
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainViewModel()
+        {
+            People = new PeopleModel
+            {
+                FirstName = "First name",
+                LastName = "Last name"
+            };
+        }
+        #endregion
+        #region Properties
+        /// <summary>
+        /// Get or set people.
+        /// </summary>
+        public PeopleModel People { get; set; }
+        #endregion
+    }
+}
