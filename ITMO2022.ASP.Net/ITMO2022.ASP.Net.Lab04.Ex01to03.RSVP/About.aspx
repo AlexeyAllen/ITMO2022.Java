a735f84b25bd0b8b00b170710dff615adb689dd6
Aleksei Allen <aqml@mail.ru>
2023-02-08 17:39:37 +0300

    ASP.Net Lab 4 added

---
+++ ITMO2022.ASP.Net/ITMO2022.ASP.Net.Lab04.Ex01to03.RSVP/About.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="ITMO2022.ASP.Net.Lab03.Ex01to02.RSVP.About" MasterPageFile="~/Site.master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <h1>Семинар</h1>
+    <h2>Приглашаем на семинар</h2>
+    <p>Напоминаем Вам, что если Вы планируете выступить с докладом, то презентация для доклада должна быть загружена до начала работы секции </p>
+    <p>Начало регистрации 1.01.2017 в 9.00.</p>
+</asp:Content>
+
+<%--<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>--%>
---
+++ ITMO2022.ASP.Net/ITMO2022.ASP.Net.Lab04.Ex01to03.RSVP/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ITMO2022.ASP.Net.Lab03.Ex01to02.RSVP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ITMO2022.ASP.Net.Lab03.Ex01to02.RSVP")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4dd6f96b-c868-4421-8e95-8d5d4b5c9c76")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
