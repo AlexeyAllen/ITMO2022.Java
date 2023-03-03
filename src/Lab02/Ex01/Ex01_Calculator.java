d8da3fbdf08be4c2e74168af7f490f027a9f491d
Aleksei Allen <aqml@mail.ru>
2023-03-03 21:51:46 +0500

    Lab2 Ex01 Calculator modified

--- src/Lab02/Ex01/Ex01_Calculator.java
+++ src/Lab02/Ex01/Ex01_Calculator.java
@@ -2,36 +2,84 @@ package Lab02.Ex01;
 
 public class Ex01_Calculator {
     public static void main(String[] args) {
-        calc(1.0, 24324L, 3, 4);
-        calc(1.0, 24324L, 3);
-        calc(1.4, 24324L, 3, 4L);
-        calc(1.3, 24324L, 3, 4.5);
-        calc(-24324L, 3);
+        sum(5,8);
+        sum(1000L,5000L);
+        sum(4.8, 4.9);
 
+        multiply(5,8);
+        multiply(1000L,5000L);
+        multiply(4.8, 4.9);
+
+        divide(5,8);
+        divide(1000L,5000L);
+        divide(4.8, 4.9);
+
+        subtract(5,8);
+        subtract(1000L,5000L);
+        subtract(4.8, 4.9);
     }
 
-    public static void calc(double a, long b, int c, int d) {
-        double e = a + b + c + d;
-        System.out.println(e);
+    //sum
+    public static void sum(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
     }
 
-    public static void calc(double a, long b, int c) {
-        double d = a / b / c;
-        System.out.println(d);
+    public static void sum(long a, long b) {
+        long c = a + b;
+        System.out.println(c);
     }
 
-    public static void calc(double a, long b, int c, long d) {
-        double e = a * b * c * d;
-        System.out.println(e);
+    public static void sum(double a, double b) {
+        double c = a + b;
+        System.out.println(c);
     }
 
-    public static void calc(double a, long b, int c, double d) {
-        double e = a - b - c - d;
-        System.out.println(e);
+    //multiply
+    public static void multiply(int a, int b) {
+        int c = a * b;
+        System.out.println(c);
     }
 
-    public static void calc(long b, int c) {
-        long e = b - c;
-        System.out.println(e);
+    public static void multiply(long a, long b) {
+        long c = a * b;
+        System.out.println(c);
+    }
+
+    public static void multiply(double a, double b) {
+        double c = a * b;
+        System.out.println(c);
+    }
+
+    //divide
+    public static void divide(int a, int b) {
+        int c = a / b;
+        System.out.println(c);
+    }
+
+    public static void divide(long a, long b) {
+        long c = a / b;
+        System.out.println(c);
+    }
+
+    public static void divide(double a, double b) {
+        double c = a / b;
+        System.out.println(c);
+    }
+
+    //subtract
+    public static void subtract(int a, int b) {
+        int c = a - b;
+        System.out.println(c);
+    }
+
+    public static void subtract(long a, long b) {
+        long c = a - b;
+        System.out.println(c);
+    }
+
+    public static void subtract(double a, double b) {
+        double c = a - b;
+        System.out.println(c);
     }
 }
