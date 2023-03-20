68ee9df73eb82cc554c003d853fb9cb96945c9b3
Aleksei Allen <aqml@mail.ru>
2023-03-20 03:29:06 +0300

    Lab09 corrected

--- src/Lab09/Ex02/Main.java
+++ src/Lab09/Ex02/Main.java
@@ -1,8 +1,22 @@
 package Lab09.Ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
+        myList.add("Volga");
+        myList.add("BMW");
+        myList.add("Renault");
+        myList.add("Volga");
+        myList.add("Mercedes");
+        myList.add("Volga");
+        myList.add("Tesla");
+
+
         NoDuplicate noDuplicate = new NoDuplicate();
-        noDuplicate.noDuplicate("Volga", "BMW", "Renault", "Volga");
+        noDuplicate.noDuplicate(myList);
     }
 }
--- src/Lab09/Ex02/NoDuplicate.java
+++ src/Lab09/Ex02/NoDuplicate.java
@@ -1,16 +1,19 @@
 package Lab09.Ex02;
 
 import java.util.HashSet;
+import java.util.List;
 
 public class NoDuplicate {
-    public void noDuplicate(String item1, String item2, String item3,String item4){
-        HashSet<String> itemList = new HashSet<>();
-        itemList.add(item1);
-        itemList.add(item2);
-        itemList.add(item3);
-        itemList.add(item4);
 
-        for (String item : itemList){
+    public void noDuplicate(List<String> stringList) {
+
+        HashSet<String> itemList = new HashSet<>();
+
+        for (int i = 0; i < stringList.size(); i++) {
+            itemList.add(stringList.get(i));
+        }
+
+        for (String item : itemList) {
             System.out.println(item);
         }
     }
